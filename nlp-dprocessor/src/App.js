bfe98b2bfebc96920e1706e14ad04aa7a9607a20
qi zhao <qiz216@outlook.com>
2023-03-19 17:24:28 -0400

    feat version ne

--- nlp-dprocessor/src/App.js
+++ nlp-dprocessor/src/App.js
@@ -98,115 +98,129 @@ function App() {
       setScroll(true)
       setUploadError(false);
     } catch (error) {
+      alert("Instruction failed!");
       setUploadError(true);
     } finally {
       setLoading(false);
     }
   };
-  
 
   const fetchData = async () => {
     setLoading(true);
+    try {
     const fetchedData = await api.fetchData();
-    setLoading(false);
     setData(fetchedData);
-  };
-
-  const handleProcessData = async (event) => {
+    } catch (error) {
+    alert("Instruction failed!");
+    } finally {
+    setLoading(false);
+    }
+    };
+    
+    const handleProcessData = async (event) => {
     event.preventDefault();
     const input = event.target.elements.instruction.value;
-
     setLoading(true);
-    await api.processData(input);
-    setLoading(false);
+try {
+  await api.processData(input);
+  fetchData();
+} catch (error) {
+  alert("Instruction failed!");
+} finally {
+  setLoading(false);
+}
+};
 
-    fetchData();
-  };
+const handleRevert = async () => {
+setLoading(true);
+try {
+await api.revertData();
+fetchData();
+} catch (error) {
+alert("Instruction failed!");
+} finally {
+setLoading(false);
+}
+};
 
-  const handleRevert = async () => {
-    setLoading(true);
-    await api.revertData();
-    setLoading(false);
+const handlePlotData = async (event) => {
+event.preventDefault();
+const input = event.target.elements.instruction.value;
+setLoading(true);
+try {
+  const imageData = await api.plotData(input);
+  const url = URL.createObjectURL(new Blob([imageData], { type: 'image/png' }));
+  setPlotUrl(url);
+} catch (error) {
+  alert("Instruction failed!");
+} finally {
+  setLoading(false);
+}
+};
 
-    fetchData();
-  };
-
-  const handlePlotData = async (event) => {
-    event.preventDefault();
-    const input = event.target.elements.instruction.value;
-
-    setLoading(true);
-    const imageData = await api.plotData(input);
-    const url = URL.createObjectURL(new Blob([imageData], { type: 'image/png' }));
-    setLoading(false);
-
-    setPlotUrl(url);
-  };
-
-  return (
-    <div className="App">
-    {loading && (
-    <ProgressBar
-      now={100}
-      animated
-      label="Loading..."
-      style={{ position: 'fixed' }}
-    />
-  )}
-  {uploadError && (
-    <Alert
-      variant="danger"
-      onClose={() => setUploadError(false)}
-      dismissible
-      style={{ position: 'fixed', zIndex: 1050, width: '100%', textAlign: 'center' }}
-    >
-      Upload Failed
-    </Alert>
-  )} 
-      {/* ProgressBar and Alert components */}
-      <Container fluid>
-        <LandingSection handleTryNow={scrollToUpload} />
-        <div ref={uploadRef}></div>
-        <UploadSection handleUpload={handleUpload} />
-
-        {uploaded && (
-          <Row>
-            <Resizable
-              defaultSize={{ width: '50%', height: 'auto' }}
-              minWidth="20%"
-              maxWidth="80%"
-              enable={{
-                top: false,
-                right: true,
-                bottom: false,
-                left: false,
-                topRight: false,
-                bottomRight: false,
-                bottomLeft: false,
-                topLeft: false,
-              }}
-              handleComponent={{ right: CustomHandle }}
-            >
-              <div ref={etlRef}>
-                <ETLSection
-                  data={data}
-                  handleProcessData={handleProcessData}
-                  handleRevert={handleRevert}
-                />
-              </div>
-            </Resizable>
-            <Col className="section resize-container">
-              <PlotSection
-                plotUrl={plotUrl}
-                handlePlotData={handlePlotData}
-              />
-            </Col>
-          </Row>
-        )}
-      </Container>
-      <footer className="footer">Developed by Qi Zhao</footer>
-    </div>
-  );
+return (
+<div className="App">
+{loading && (
+<ProgressBar
+now={100}
+animated
+label="Loading..."
+style={{ position: 'fixed' }}
+/>
+)}
+{uploadError && (
+<Alert
+variant="danger"
+onClose={() => setUploadError(false)}
+dismissible
+style={{ position: 'fixed', zIndex: 1050, width: '100%', textAlign: 'center' }}
+>
+Upload Failed
+</Alert>
+)}
+{/* ProgressBar and Alert components */}
+<Container fluid>
+<LandingSection handleTryNow={scrollToUpload} />
+<div ref={uploadRef}></div>
+<UploadSection handleUpload={handleUpload} />
+{uploaded && (
+      <Row>
+        <Resizable
+          defaultSize={{ width: '50%', height: 'auto' }}
+          minWidth="20%"
+          maxWidth="80%"
+          enable={{
+            top: false,
+            right: true,
+            bottom: false,
+            left: false,
+            topRight: false,
+            bottomRight: false,
+            bottomLeft: false,
+            topLeft: false,
+          }}
+          handleComponent={{ right: CustomHandle }}
+        >
+          <div ref={etlRef}>
+            <ETLSection
+              data={data}
+              handleProcessData={handleProcessData}
+              handleRevert={handleRevert}
+            />
+          </div>
+        </Resizable>
+        <Col className="section resize-container">
+          <PlotSection
+            plotUrl={plotUrl}
+            handlePlotData={handlePlotData}
+          />
+        </Col>
+      </Row>
+    )}
+  </Container>
+  <footer className="footer">Developed by Qi Zhao</footer>
+</div>
+);
 }
 
 export default App;
